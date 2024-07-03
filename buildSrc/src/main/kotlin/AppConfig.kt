17f060fb67ff0203f9bb26836e111c4bdcef6a2c
Loococo <yehddnjs@gmail.com>
2024-07-04 08:41:55 +0900

    fix: downgrade minSdk version to 26

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val applicationId = "app.loococo.imagesearch"
     const val compileSdk = 34
     const val buildToolsVersion = "34.0.0"
-    const val minSdk = 29
+    const val minSdk = 26
     const val targetSdk = 34
     const val versionCode = 1
     const val versionName = "0.0.1"
